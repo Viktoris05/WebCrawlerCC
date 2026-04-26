ca93ff15c000c6ace01aaf6381c48a588cfcfa19
vladichok <vladiki1801@gmail.com>
2026-04-26 19:58:15 +0200

    Remove unused import

--- src/main/java/at/aau/cc/WebCrawler.java
+++ src/main/java/at/aau/cc/WebCrawler.java
@@ -4,7 +4,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.util.List;
-import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Stack;
