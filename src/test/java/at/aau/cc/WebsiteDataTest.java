963db97906d47c5518cb871ab7b6393bf4480c71
Viktoris05 <viktoris@edu.aau.at>
2026-04-27 17:22:54 +0200

    Implement WebSiteDataTest and make WebsiteData a Record class

--- src/test/java/at/aau/cc/WebsiteDataTest.java
+++ src/test/java/at/aau/cc/WebsiteDataTest.java
@@ -1,50 +1,73 @@
 package at.aau.cc;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static java.lang.Integer.parseInt;
+
 
 class WebsiteDataTest {
 
-    @BeforeAll
-    static void setUp(){
-        String[][] headersOne = {{"Hello there!", "h1"}, {"General Kenobi!", "h2"}, {"You are a bold one.", "h3"}};
-        WebsiteData websiteOne = new WebsiteData("",1,headersOne);
+    String testLink;
+    int testDepth;
+    String testHeader;
+    int testHeaderTag;
+    String[][] testHeaders;
 
-        String[][] headersTwo = {{"I don't like sand.", "h1"}, {"It's rough,", "h2"}, {"coarse", "h2"}, {"and it gets everywhere", "h2"}};
-        WebsiteData websiteTwo = new WebsiteData("",2,headersTwo);
-
-        String[][] headersThree = {{"My Powers have doubled since the last time we met, Count!", "h1"}, {"Good, twice the pride, double the fall.", "h5"}};
-        WebsiteData websiteThree = new WebsiteData("",3,headersThree);
+    WebsiteData testWebsiteData;
+    void createTestData() {
+        testLink = "testlink.com";
+        testDepth = 2;
+        testHeader = "Hello there!";
+        testHeaderTag = 1;
+        testHeaders = new String[][]{{testHeader, "1"}, {"General Kenobi!", "2"}, {"You are a bold one.", "3"}};
+        testWebsiteData = new WebsiteData(testLink,testDepth, testHeaders);
     }
+
 
     @Test
     void getLink() {
+        createTestData();
 
-    }
+        var result = testWebsiteData.link();
 
-    @Test
-    void setLink() {
+        Assertions.assertEquals(testLink, result);
     }
 
     @Test
     void getCurrentDepth() {
-    }
+        createTestData();
 
-    @Test
-    void setCurrentDepth() {
+        var result = testWebsiteData.currentDepth();
+
+        Assertions.assertEquals(testDepth, result);
     }
 
     @Test
     void getHeaders() {
+        createTestData();
+
+        var result = testWebsiteData.headers();
+
+        Assertions.assertArrayEquals(testHeaders, result);
     }
 
     @Test
     void getHeader() {
+        createTestData();
+
+        var result = testWebsiteData.getHeader(0);
+
+        Assertions.assertEquals(testHeader, result);
     }
 
     @Test
-    void setHeaders() {
+    void getHeaderTag() {
+        createTestData();
+        int inputHeader = parseInt(testHeaders[0][1]);
+
+        var result = testWebsiteData.getHeaderTag(0);
+
+        Assertions.assertEquals(inputHeader, result);
     }
 }
